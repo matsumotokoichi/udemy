063569176eb2d2a931080c10d0b1662e25d88716
matsumotokoichi <163325930+matsumotokoichi@users.noreply.github.com>
2024-08-17 20:25:06 +0900

    person更新
    git練習

--- section7/ModelProject/ModelApp/models.py
+++ section7/ModelProject/ModelApp/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    birthday = models.DateField(default='1900-01-01')
+    email = models.EmailField(db_index=True)
+    salary = models.FloatField(null=True)
+    memo = models.TextField()
+    web_site = models.URLField(null=True, blank=True)
+    create_at = models.DateTimeField(default=timezone.datetime.now())
+    update_at = models.DateTimeField()
     
-class Sales(models.Model):
-    fee = models.IntegerField()
