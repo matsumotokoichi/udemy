b81f0dd5151e54cfd0d5af5b21385b38f05184ed
matsumotokoichi <163325930+matsumotokoichi@users.noreply.github.com>
2024-08-19 00:20:55 +0900

    Update base.py

--- 文法/base.py
+++ 文法/base.py
@@ -2,5 +2,18 @@
 
 def print_hello():
     print('hello')
+ 
+def num_max(a, b):
+    if a > b:
+        return a
+    else :
+        return b
     
-print_hello()
+def num_min(a, b):
+    if a > b:
+        print(b)
+    else :
+        print(a)
+    
+print(num_max(1,2))
+num_min(1,2)
