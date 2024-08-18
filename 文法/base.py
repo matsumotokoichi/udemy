4770b39f6df9421e151fd12f0a7d3881addd8500
matsumotokoichi <163325930+matsumotokoichi@users.noreply.github.com>
2024-08-19 00:07:29 +0900

    Update base.py

--- 文法/base.py
+++ 文法/base.py
@@ -1,17 +1,6 @@
-# 
-class MyException(Exception):
-    pass
+# 関数
 
-def devide(a, b):
-    if b == 0 :
-        raise MyException('0では割り切れません')
-    else:
-        return a/b
+def print_hello():
+    print('hello')
     
-try:
-    c = devide(1,0)
-except Exception as e:
-    print(e, type(e))
-    
-    
-# この文章をコミットします
+print_hello()
