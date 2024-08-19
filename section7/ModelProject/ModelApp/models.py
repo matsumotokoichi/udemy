55b0a7e7a6de3dfb358bf98bbfbcc0e6b76169a8
matsumotokoichi <163325930+matsumotokoichi@users.noreply.github.com>
2024-08-19 18:38:45 +0900

    models
    git練習

--- section7/ModelProject/ModelApp/models.py
+++ section7/ModelProject/ModelApp/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
+class BaseMeta(models.Model):
+    
+    create_at = models.DateTimeField(default=timezone.datetime.now())
+    update_at = models.DateTimeField()
+    
+    class Meta:
+        abstract = True
 
-class Person(models.Model):
+
+class Person(BaseMeta):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     birthday = models.DateField(default='1900-01-01')
@@ -10,6 +18,8 @@ class Person(models.Model):
     salary = models.FloatField(null=True)
     memo = models.TextField()
     web_site = models.URLField(null=True, blank=True)
-    create_at = models.DateTimeField(default=timezone.datetime.now())
-    update_at = models.DateTimeField()
     
+    class Meta:
+        db_table = 'person'
+        index_together = [['first_name', 'last_name']]
+        ordering = ['salary']
